566f4230f830747b840b42a2f293b2b566d8b585
UFUKYIL <ufukyil74@gmail.com>
2022-07-31 22:23:03 +0300

    Ufuk branch work1

--- src/feature_Metka/Test_Ufuk.java
+++
@@ -1,7 +0,0 @@
-package feature_Metka;
-
-public class Test_Ufuk {
-    public static void main(String[] args) {
-        System.out.println("Ufuk branch work1");
-    }
-}
---
+++ src/ufuk/Test.java
@@ -0,0 +1,9 @@
+package ufuk;
+
+public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Ufuk work1");
+    }
+
+}
