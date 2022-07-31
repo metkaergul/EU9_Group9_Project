59b0f00b6cb937ed75a544a71b8e44865cb72663
BayramBilal <bilalbayramb22@gmail.com>
2022-07-31 22:18:07 +0300

    updated test 2

--- src/feature_Metka/Test2_bilal.java
+++ src/feature_Metka/Test2_bilal.java
@@ -4,6 +4,12 @@ public class Test2_bilal {
     public static void main(String[] args) {
         String group = "Hello";
         System.out.println(group);
+
+
+
+        int a = 2;
+        int b = 5;
+        System.out.println(a+b);
     }
 
 
